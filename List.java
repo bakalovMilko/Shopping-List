a6bc9770adbaed5882281db06004799b20a6c032
bakalovMilko <71628681+bakalovMilko@users.noreply.github.com>
2021-01-23 17:53:10 +0200

    Step 5.2 - TODO Functionality => DONE

--- List.java
+++ List.java
@@ -3,9 +3,8 @@ package shoppingListGUI;
 import java.util.*;
 
 public class List {
-	private String name = "New list";
+	private String name = "";
 	private ArrayList <Product> productsInTheList = new ArrayList<>();
-	private int numberOfProducts = 0;
 	
 	List(){
 		
@@ -21,27 +20,32 @@ public class List {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getNumberOfProducts() {
-		return numberOfProducts;
+	public ArrayList<Product> getProductsInTheList() {
+		return this.productsInTheList;
 	}
 	
 	public void addProduct (String productName) {
-		if(productName == "") {
-			return;
+		int index = this.searchForProduct(productName);
+		if(index>-1) {
+			 //add 1 to timesInList for this product
+			productsInTheList.get(index).setTimesInList(productsInTheList.get(index).getTimesInList()+1);
 		}
-		Product product = new Product(productName);
-		productsInTheList.add(product);
-		numberOfProducts = getNumberOfProducts() + 1;
+		else {
+			Product product = new Product(productName);
+			productsInTheList.add(product);
+		}
 	}
 	//function that searches for particular product in the list
 	//if the product is found return its index
 	//else return -1
 	public int searchForProduct (String productName) {
+		if(productsInTheList.size()==0) return -1;
 		int index=0;
-		while(!productsInTheList.get(index).getName().equals(productName) && getNumberOfProducts()>index ) {
+		while(!productsInTheList.get(index).getName().equals(productName) && productsInTheList.size()>index ) {
 			index++;
+			if(index == productsInTheList.size()) break;
 		}
-		if(index==getNumberOfProducts()) {
+		if(index==productsInTheList.size()) {
 			return -1;
 		}
 		else {
@@ -50,29 +54,38 @@ public class List {
 	}
 	public void deleteProduct(String productName) {
 		int index = this.searchForProduct(productName);
-		productsInTheList.remove(index);
-		numberOfProducts = getNumberOfProducts() - 1;
+		if(index>=0) {
+			if(productsInTheList.get(index).getTimesInList()>1) {
+				productsInTheList.get(index).setTimesInList(productsInTheList.get(index).getTimesInList()-1);
+			}
+			else {
+				productsInTheList.remove(index);
+			}
+		}
+		else System.out.println("There is no product with this name in the list.");
 	}
 	//function that returns the products on different rows with numbering
 	public String toString() {
 		String a = "";
-		for(int i=0; i<getNumberOfProducts(); i++) {
-			a += (i+1) + ". " + productsInTheList.get(i).getName() + "\n";
+		for(int i=0; i<productsInTheList.size(); i++) {
+			a += (i+1) + ". " + productsInTheList.get(i).getName() 
+		 + ((productsInTheList.get(i).getTimesInList()>1)? (" x " + productsInTheList.get(i).getTimesInList()) : "") + "\n";
 		}
 		return a;
 	}
 	//function that return the products on different rows
-	public String toFile() {
+	public String toFileFormat() {
 		String a = "";
-		for(int i=0; i<getNumberOfProducts(); i++) {
-			a+= productsInTheList.get(i).getName() + "\n";
+		for(int i=0; i<productsInTheList.size(); i++) {
+			for(int j=0; j<productsInTheList.get(i).getTimesInList(); j++) {
+				a+= productsInTheList.get(i).getName() + "\n";
+			}
 		}
 		return a;
 	}
 	public void makeListEmpty () {
-		while(numberOfProducts>0) {
+		while(productsInTheList.size()>0) {
 			productsInTheList.remove(0);
-			numberOfProducts--;
 		}
 	}
 }
